d7b168060739b46a8ccd3e8d3cb3e04d3c5acb56
Boset <boss-x86@protonmail.com>
2023-10-26 15:15:47 -0600

    actualizando modelo y vista

--- herramientasweb_api/models.py
+++ herramientasweb_api/models.py
@@ -12,6 +12,14 @@ class BearerTokenAuthentication(TokenAuthentication):
 class Profiles(models.Model):
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    matricula = models.CharField(max_length=255,null=True, blank=True)
+    curp = models.CharField(max_length=255,null=True, blank=True)
+    rfc = models.CharField(max_length=255,null=True, blank=True)
+    #fecha_nacimiento = models.CharField(max_length=255,null=True, blank=True)
+    fecha_nacimiento = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    edad = models.IntegerField(null=True, blank=True)
+    telefono = models.CharField(max_length=255, null=True, blank=True)
+    ocupacion = models.CharField(max_length=255,null=True, blank=True)
     creation = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     update = models.DateTimeField(null=True, blank=True)
 
